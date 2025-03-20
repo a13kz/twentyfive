abf1da43f322b49a8b2705c7b89b23452ed876a0
a13kz <nils.alexander.bohre@gmail.com>
2025-03-20 18:03:59 +0100

    fixat hover funktion och ändrat about till buy page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 document.querySelector(".menu-button").addEventListener("click", showMenu)
+document.querySelector(".buy-item").addEventListener("click", addToList)
 
+itemContainer = []
 
 function showMenu(){
     let menu = document.querySelector("nav.menu")
@@ -7,4 +9,8 @@ function showMenu(){
 
     body.classList.toggle("disable-scroll")
     menu.classList.toggle("show")
+}
+
+function addToList(){
+    postMessage("item has been added to cart")
 }
